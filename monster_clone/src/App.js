4fbd4a8ee82ce32c1e32b502ea7a213d7b6fd0d6
Hirak-Debadhikary <103572541+Hirak-Debadhikary@users.noreply.github.com>
2022-09-28 23:02:41 +0530

    day_2 Work

---
+++ monster_clone/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import HomePage from "./Components/HomePage";
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return <div className="App">
+    <Navbar/>
+    <HomePage/>
+  </div>;
+}
+
+export default App;
